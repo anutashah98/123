a2dc940bcdab7004f1493ef9b72f53c13180a21b
anutashah98 <79895916+anutashah98@users.noreply.github.com>
2023-04-09 11:32:31 +0300

    Back

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using DefaultNamespace.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private LevelsConfigs _levelsConfigs;
+        [SerializeField] private GameStateManager _gameStateManager;
+
+        private void Awake()
+        {
+            _gameStateManager.OnWin += SetCurrentLevelCompleted;
+        }
+
+        private void SetCurrentLevelCompleted()
+        {
+            var currentSceneName = SceneManager.GetActiveScene().name;
+
+            foreach (var level in _levelsConfigs.Levels)
+            {
+                if (level.SceneName == currentSceneName)
+                {
+                    level.Completed = true;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.UI
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private RectTransform[] _windows;
+
+        public void OpenWindow(string windowName)
+        {
+            foreach (var window in _windows)
+            {
+                if (window.name == windowName)
+                {
+                    window.gameObject.SetActive(true);
+                }
+                else
+                {
+                    window.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
